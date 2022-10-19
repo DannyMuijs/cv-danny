87200a023844cbb856e2a2c134fe46e52b388e94
Danny Muijs <107554347+DannyMuijs@users.noreply.github.com>
2022-10-19 11:41:51 +0200

    Delete src directory

--- src/components/Contact/Contact.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import FormContact from "./contactform";
-import Grid from "@mui/material/Unstable_Grid2";
-
-export default function Contact() {
-  return <FormContact />;
-}
